af14a96b361de7718df238b4320475863af17a40
shufo <meikyowise@gmail.com>
2017-06-16 21:27:34 +0900

    Bump payjp to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Payjp.Mixfile do
 
   def project do
     [app: :payjp,
-     version: "0.1.0",
+     version: "0.1.2",
      elixir: "~> 1.4",
      description: description(),
      build_embedded: Mix.env == :prod,
