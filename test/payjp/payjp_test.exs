a120e8c525fb6910f0f30808ab9e417eb8e690c6
shufo <meikyowise@gmail.com>
2021-01-03 19:01:40 +0900

    refactor: 💡 delete unnecessary lines

--- test/payjp/payjp_test.exs
+++ test/payjp/payjp_test.exs
@@ -1,21 +1,12 @@
 defmodule Payjp.PayjpTest do
   use ExUnit.Case
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
-  import Mock
 
   @moduletag :payjp
 
   test "process_url for v1" do
     assert Payjp.process_url("payment") == "https://api.pay.jp/v1/payment"
   end
-
-  # test "make_request_with_key fails when no key is supplied on environment config" do
-  #   with_mock System, [get_env: fn(_opts) -> nil end] do
-  #     assert_raise Payjp.MissingSecretKeyError, fn ->
-  #       Payjp.config_or_env_key
-  #     end
-  #   end
-  # end
 
   test "make_request_with_key fails when no key is supplied on payjp request" do
     use_cassette "payjp_test/invalid_key_request", match_requests_on: [:query, :request_body] do
